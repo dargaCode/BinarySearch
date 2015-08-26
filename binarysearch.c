294dec69997da292bf3195c00a9bc30991d9e352
dargaCode <darga.code@gmail.com>
2015-08-26 19:51:13 -0400

    Generate random search term to find within the array

--- binarysearch.c
+++ binarysearch.c
@@ -4,7 +4,7 @@
 
 // prototypes
 void fill_array(int arr[], int size);
-
+int get_random_term(int length);
 
 int main (int argc, string argv[])
 {
@@ -15,15 +15,17 @@ int main (int argc, string argv[])
         return 1;
     }
 
-    int size = atoi(argv[1]);
-    int data[size];
-    fill_array(data, size);
+    int len = atoi(argv[1]);
+    int data[len];
+    fill_array(data, len);
 
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < len; i++)
     {
     printf("%i \n", data[i]);
     }
 
+    int term = get_random_term(len);
+    printf("search term %i \n", term);
 
     return 0;
 
@@ -33,12 +35,12 @@ int main (int argc, string argv[])
 /*
  * Fill an array with ints, with a chance of skipping
  */
-void fill_array(int arr[], int size)
+void fill_array(int arr[], int len)
 {
     int threshold = 20;
     srand(time(NULL));
 
-    for (int i = 0, j = 0; i < size; i++)
+    for (int i = 0, j = 0; i < len; i++)
     {
         int random;
         do
@@ -50,4 +52,12 @@ void fill_array(int arr[], int size)
 
         arr[i] = j;
     }
+}
+
+/*
+ * Generate the int to search for in the array
+ */
+int get_random_term(int len)
+{
+    return rand() % len;
 }
